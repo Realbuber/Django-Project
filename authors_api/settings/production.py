715640bb672fbe55257bd3572320dbf4d36baec5
Realbuber <realbuber@RealbuberdeMacBook-Air.local>
2023-12-04 04:08:52 +0800

    setup the dockerfile entry and compose

---
+++ authors_api/settings/production.py
@@ -0,0 +1,8 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("Realbuber", "api.ms0345313@gmail.com")]
+
+# TODO add domain names of the prodcution server
+
+CSRD_TRUSTED_ORIGINS = ["http://localhost:8080"]
