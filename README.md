becae4333e613b8148e9763018f7d474f5968183
Realbuber <realbuber@RealbuberdeMacBook-Air.local>
2023-12-06 17:55:43 +0800

    complete migrate and set up the admin page

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Django-Project
+step:
+if you are a Mac user and your chip is M1 or M2, then you need to enter this command first:
+-> export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
+1.build the Docker
+run command -> docker-compose -f local.yml up --build -d --remove-orphans
+
+2.
+make migration
+run command -> docker-compose -f local.yml run --rm api python manage.py makemigrations
+
+3.migrate
+run command -> docker-compose -f local.yml run --rm api python manage.py migrate
+
--- authors_api/urls.py
+++ authors_api/urls.py
@@ -1,7 +1,29 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="MECO API",
+        default_version="v1",
+        description="MECO API endpoints.",
+        contact=openapi.Contact(email="ms0345313@gmail.com"),
+        license=openapi.License(name="MIT license"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header = "MECO API Admin"
+
+admin.site.site_title = "MECO API Admin Portal"
+
+admin.site.index_title = "Welcome to MECO API Portal"
